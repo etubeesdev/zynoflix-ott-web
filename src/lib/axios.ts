24b7d74f98aed8933ef881d948c5cbc57a4e575e
Gokula krishnan R <105733059+gokula-krishnan-r-dev@users.noreply.github.com>
2024-06-14 19:28:40 +0530

    chore: Update axios base URL to use dynamic server address and dotenv for environment variables

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,17 @@
 // @ts-nimport Axios from "axios";
 import Axios from "axios";
 import { accessToken } from "./user";
+import dotenv from "dotenv";
+dotenv.config();
 
-const SERVER = "http://65.0.55.141:8080";
+console.log(process.env.BASE_API_URL, "api");
+
+const SERVER =
+  process.env.BASE_API_URL === "development"
+    ? "http://localhost:8080"
+    : "http://65.0.55.141:8080";
+    
+// const SERVER = "http://65.0.55.141:8080";
 // const SERVER = "http://localhost:8080";
 
 const axios = Axios.create({
