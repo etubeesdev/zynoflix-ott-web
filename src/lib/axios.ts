b9ba4ccedadf847f12eea44b6856a1d8f4ab6f02
Gokula krishnan R <105733059+gokula-krishnan-r-dev@users.noreply.github.com>
2024-06-14 19:23:02 +0530

    chore: update axios base URL to use dynamic server address

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,12 @@
 // @ts-nimport Axios from "axios";
 import Axios from "axios";
-import { access } from "fs";
 import { accessToken } from "./user";
+
+const SERVER = "http://65.0.55.141:8080";
+// const SERVER = "http://localhost:8080";
+
 const axios = Axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: `${SERVER}/api`,
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${
